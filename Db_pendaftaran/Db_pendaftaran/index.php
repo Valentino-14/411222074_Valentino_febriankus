2d9a8009dd5474f250db4fa743803bd5379cc44c
Valentino-14 <74384962+Valentino-14@users.noreply.github.com>
2025-04-26 16:06:10 +0700

    Add files via upload

---
+++ Db_pendaftaran/Db_pendaftaran/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pendaftaran Siswa Baru | SMK UNDIRA</title>
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"> 
+</head>
+<body>
+    <div class="container py-4">
+        <header class="jumbotron text-center mb-4">
+            <h3>Pendaftaran Siswa Baru</h3>
+            <h3>SMK UNDIRA</h3>
+        </header>
+        <div>
+        <h4>MENU</h4>
+        </div>
+    <div class="text-center">
+    <ul class="list-group">
+            <li class="list-group-item">
+                <a href="form-daftar.php">Form daftar</a>
+            </li>
+        <li class="list-group-item">
+                <a href="list-siswa.php"> Data Pendaftaran</a>
+        </li>
+        </ul>
+    </div>
+        
+    </div>
+    
+</body>
+</html>
